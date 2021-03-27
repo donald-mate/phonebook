cf6c08723fdb96193f4ffcff78305e1be3739fbb
Donald Mate <donald.mate@gmail.com>
2021-03-26 20:06:26 -0400

    styled error message to red

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -3,13 +3,19 @@ import React from 'react'
 const Notification = ({ message }) => {
     if (message === null) {
       return null
+    } else if (message.includes("removed")) {
+        return (
+            <div className="error">
+              {message}
+            </div>
+        )
+    } else {
+        return (
+            <div className="added">
+              {message}
+            </div>
+        )
     }
-  
-    return (
-      <div className="added">
-        {message}
-      </div>
-    )
   }
 
 export default Notification
